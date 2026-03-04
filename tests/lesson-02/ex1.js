a1ea6c190e01c16e0ecfee2eda89db67299d249b
ngochan.do <hanvan1331@gmail.com>
2026-03-05 00:32:49 +0700

    Feat : Lesson02 - Exercises - JavaScript

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,19 @@
+/*
+1. Tạo file ex1.js để giải bài sau:
+a. Khai báo một hằng số number với giá trị là 12.
+b. Khai báo một biến name với giá trị là ”my number”
+c. Khai báo một biến isEven với giá trị là sai.
+d. In ra kiểu dữ liệu của các biến đã khai báo, sử dụng typeof
+*/
+
+const a = 12;
+let name = "my number";
+let isEven = false;
+
+console.log(typeof a);
+console.log(typeof name);
+console.log(typeof isEven);
+
+
+
+
---
+++ tests/lesson-02/ex3.js
@@ -0,0 +1,18 @@
+/*
+Tạo file ex3.js, thêm vào đáp án cho câu hỏi sau: Cho 2 biến: a = true, b = false, c =
+true. Theo bạn, kết quả của các biểu thức sau là gì?
+a. a && b && c 
+b. a && b || c
+*/
+
+let a = true;
+let b = false;
+let c = true;
+
+/*
+Đáp án: 
+a) a && b && c - Output: false (a && b = false -> false && c -> fasle)
+b) a && b || c - Output: true (a && b = false -> false || c -> true)
+*/
+
+
